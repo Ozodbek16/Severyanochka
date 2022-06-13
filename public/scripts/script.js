fe86ec0a63981fba52b6db68d1b9e44fc609516b
Muhriddin33 <muhriddinme03@gmail.com>
2022-06-13 21:20:14 +0500

    navbar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,29 +1,33 @@
+var val_0 = document.querySelector(".filtr_bottom_price button");
 
+var elem = document.querySelector(".multi-range .price_first1");
+var target = document.querySelector(".price_first");
 
-var val_0 = document.querySelector('.filtr_bottom_price button');
+let bars = document.querySelector(".bars");
+let btnbars = document.querySelector(".btn-bars");
+// bars.style.display = "none";
 
-var elem = document.querySelector('.multi-range .price_first1');
-var target = document.querySelector('.price_first');
+btnbars.addEventListener("click", () => {
+ 
+});
 
-var rangeValue = function(){
+var rangeValue = function () {
   var newValue = elem.value;
   target.value = newValue;
-}
+};
 
 elem.addEventListener("input", rangeValue);
 
+var elem2 = document.querySelector(".multi-range .price_lest1");
+var target2 = document.querySelector(".price_lest");
 
-var elem2 = document.querySelector('.multi-range .price_lest1');
-var target2 = document.querySelector('.price_lest');
-
-var rangeValue2 = function(){
+var rangeValue2 = function () {
   var newValue2 = elem2.value;
   target2.value = newValue2;
-
-}
+};
 elem2.addEventListener("input", rangeValue2);
 
 val_0.addEventListener("click", function () {
-    target.value = 0
-    target2.value = 100
+  target.value = 0;
+  target2.value = 100;
 });
