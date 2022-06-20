b12d22226a0c369fe26721cadb9f857da70a794a
BoburF <changeitgo3@gmail.com>
2022-06-20 17:29:19 +0500

    feat: navbar

---
+++ public/script/nav-script.js
@@ -0,0 +1,14 @@
+const btnCatalog = document.querySelector('.btn_catalog');
+const btnHoverCatalog = document.querySelector('.hover_catalog');
+const imgBtnCatalog = document.querySelector('.btn_catalog_img');
+
+btnCatalog.addEventListener('click', () => {
+    if(!btnHoverCatalog.classList.contains('db')){
+        btnHoverCatalog.style.display = 'block'
+        return btnHoverCatalog.classList.add('db')
+    }
+    if(btnHoverCatalog.classList.contains('db')){
+        btnHoverCatalog.style.display = 'none'
+        btnHoverCatalog.classList.remove('db')
+    }
+})
