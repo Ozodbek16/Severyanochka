80d5675989e8d1a19b15330bda24869618b765cc
baxromxoja16 <changeitgo3@gmail.com>
2022-07-13 19:44:46 +0500

    fix: user and admin login

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,57 +1,56 @@
 const express = require("express");
 const router = express.Router();
-const Mongo = require('../model/Mongo')
-const Shopping = require('../model/Shopping')
-const User = require('../model/User')
-const bcrypt = require('bcrypt')
-
+const Mongo = require("../model/Mongo");
+const Shopping = require("../model/Shopping");
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 router.get("/", async (req, res) => {
-    const pro = await Shopping.find()
-    let sum = 0
-    pro.forEach(item => {
-        sum = sum + item.count
-    })
-    const products = await Mongo.find()
-    res.render('home', {
-        title: 'Home page',
-        products,
-        sum,
-        user: res.locals.user
-    })
+  const pro = await Shopping.find();
+  let sum = 0;
+  pro.forEach((item) => {
+    sum = sum + item.count;
+  });
+  const products = await Mongo.find();
+  res.render("home", {
+    title: "Home page",
+    products,
+    sum,
+    user: res.locals.user
+  });
 });
 
 router.post("/login", async (req, res) => {
-    try {
-      if (!req.body) {
-        res.send("Login required");
-        // res.redirect('/api/login')
-      }
-  
-      const user = await User.findOne({ phone: req.body.phone });
-  
-      if (!user) {
-        res.send("Username is incorrect");
-        return;
-      }
-  
-      const areSame = await bcrypt.compare(req.body.password, user.password);
-  
-      if (!areSame) {
-        res.send("Password is incorrect");
-        return;
-      }
-  
-      req.session.userAuth = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        if (err) throw err;
-        res.redirect("/");
-      });
-    } catch (error) {
-      console.log(error);
-      res.redirect("/");
+  try {
+    if (!req.body) {
+      res.send("Login required");
+      // res.redirect('/api/login')
     }
-  });
+
+    const user = await User.findOne({ phone: req.body.phone });
+
+    if (!user) {
+      res.send("Username is incorrect");
+      return;
+    }
+
+    const areSame = await bcrypt.compare(req.body.password, user.password);
+
+    if (!areSame) {
+      res.send("Password is incorrect");
+      return;
+    }
+
+    req.session.userAuth = true;
+    req.session.user = user;
+    req.session.save((err) => {
+      if (err) throw err;
+      res.redirect("/");
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+});
 
 module.exports = router;
